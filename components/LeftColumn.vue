ee9803d8c0915c3ef9e397bdda863502bebbe01b
Tanat <ttanat@protonmail.com>
2022-12-26 12:28:15 +0700

    Create LeftColumn component

---
+++ components/LeftColumn.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    v-for="(link, i) in links"
+    :key="i"
+    class="mx-2"
+  >
+    <v-btn
+      variant="text"
+      size="large"
+      block
+      class="left-link"
+      :to="link.url"
+    >
+      {{ link.title }}
+    </v-btn>
+  </div>
+  <div class="mx-2">
+    <v-btn
+      class="mt-3 tweet-btn"
+      size="large"
+      color="primary"
+      rounded="pill"
+      block
+    >
+      Tweet
+    </v-btn>
+  </div>
+</template>
+
+<script setup>
+  const links = ref([
+    { title: "Explore",  url: "/explore" },
+    { title: "Notifications", url: "/notifications" },
+    { title: "Messages", url: "/messages" },
+    { title: "Bookmarks", url: "/bookmarks" },
+    { title: "Profile", url: "/profile" },
+    { title: "Settings", url: "/settings" },
+  ])
+</script>
+
+<style scoped>
+.left-link {
+  justify-content: start;
+}
+.left-link, .tweet-btn {
+  text-transform: unset;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <v-col lg="3" class="d-none d-lg-block">
+  <v-col lg="2" class="d-none d-lg-block">
+    <LeftColumn />
   </v-col>
-  <v-col sm="8" md="7" lg="5">
+  <v-col sm="8" md="7" lg="6">
     <TweetList />
   </v-col>
   <v-col md="4" lg="3" class="d-none d-md-block">
@@ -12,20 +13,5 @@
 .v-col {
   padding: unset;
   border: solid 1px red;
-}
-.nav-component {
-  height: 36px;
-}
-#nav-title {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: auto;
-}
-#app-search-bar {
-  flex: 1;
-  border-radius: 18px;
-  border: solid thin grey;
-  outline: none;
 }
 </style>
