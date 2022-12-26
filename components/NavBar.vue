dbdb14b59c396ba95755864347a2fe8ce64f3e17
Tanat <ttanat@protonmail.com>
2022-12-26 12:27:26 +0700

    Create NavBar component

---
+++ components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-col lg="2" class="nav d-none d-lg-block">
+    <v-container fluid>
+      <v-row align="center" class="px-2 py-1">
+        <div class="text-h6 nav-component" id="nav-title">Twitter</div>
+      </v-row>
+    </v-container>
+  </v-col>
+  <v-col sm="8" md="7" lg="6" class="nav">
+    <v-container fluid>
+      <v-row align="center" class="px-2 py-1">
+        <input
+          placeholder="Search"
+          id="app-search-bar"
+          class="px-4 nav-component"
+        >
+      </v-row>
+    </v-container>
+  </v-col>
+  <v-col md="4" lg="3" class="nav d-none d-md-block">
+    <v-container fluid>
+      <v-row justify="end" align="center" class="px-2 py-1">
+        <div class="nav-component">
+          <v-btn rounded="pill" class="mr-3">Login</v-btn>
+          <v-btn color="primary" rounded="pill">Sign Up</v-btn>
+        </div>
+      </v-row>
+    </v-container>
+  </v-col>
+</template>
+
+<style scoped>
+.v-col {
+  padding: unset;
+  border: solid 1px green;
+}
+#nav-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+}
+.nav-component {
+  height: 36px;
+}
+#app-search-bar {
+  flex: 1;
+  border-radius: 18px;
+  border: solid thin grey;
+  outline: none;
+  background: black;
+}
+</style>
