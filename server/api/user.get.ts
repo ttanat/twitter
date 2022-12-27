03ed69a904c50b0357dd9fdbcb8348fc85563100
Tanat <ttanat@protonmail.com>
2022-12-27 12:08:15 +0700

    Fix bug

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -2,7 +2,7 @@ import User from "@/server/models/user"
 
 export default eventHandler(async event => {
   const { username, password } = getQuery(event)
-  const user = await User.findOne({ username, password: '123' }).exec()
+  const user = await User.findOne({ username, password }).exec()
 
   if (user === null) {
     return createError({ statusCode: 404, statusMessage: "Username or password incorrect" })
