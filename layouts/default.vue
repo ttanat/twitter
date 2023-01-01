69e352c68025026f629d6f89c9ac644f73dd50be
Tanat <ttanat@protonmail.com>
2023-01-01 23:36:04 +0700

    Move nav bar to right place

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
+  <HeaderNavBar />
   <v-main>
     <v-container fluid style="max-width: 1280px">
       <v-row justify="center">
-        <HeaderNavBar />
         <slot></slot>
       </v-row>
     </v-container>
