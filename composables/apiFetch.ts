9143b6a3c33d572ea950d9d7f5a362239a07f83b
Tanat <ttanat@protonmail.com>
2023-01-03 13:59:41 +0700

    New fetch method with default options

---
+++ composables/apiFetch.ts
@@ -0,0 +1,16 @@
+export const apiFetch = async (url: string, options?: any): Promise<any> => {
+  // Default options
+  options = { ...options, server: options?.server || false, credentials: "omit" }
+
+  // Auth header
+  const { $auth } = useNuxtApp()
+  if ($auth.loggedIn()) {
+    options.headers = options.headers || {}
+    options.headers.authorization = `Bearer ${$auth.getAccessToken()}`
+  }
+
+  // Make request
+  const { data, pending, error, refresh } = await useFetch(url, options)
+
+  return { data, pending, error, refresh }
+}
