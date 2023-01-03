f700ba134a0bd24715f1973e98157f0e3cf72406
Tanat <ttanat@protonmail.com>
2023-01-03 13:31:41 +0700

    Logout page

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,3 +1,9 @@
+<template>
+  <div class="justify-center align-self-center">
+    <v-progress-circular indeterminate></v-progress-circular>&ensp;<span class="text-h6">Logging out...</span>
+  </div>
+</template>
+
 <script setup>
   useNuxtApp().$auth.logout()
 </script>
