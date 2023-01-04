54c173089c4f4932bb4cd0390161a9f3863dded1
Tanat <ttanat@protonmail.com>
2023-01-05 00:21:05 +0700

    Hash password

--- server/api/auth/token.post.ts
+++ server/api/auth/token.post.ts
@@ -1,19 +1,21 @@
 import User from "@/server/models/user"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const config = useRuntimeConfig()
 
 export default eventHandler(async event => {
   const { username, password } = await readBody(event)
   // Find user in database
-  const user = await User
-    .findOneAndUpdate({ username, password }, { lastActive: new Date() })
-    .select({ username: 1, _id: 0 })
-    .exec()
+  const user = await User.findOne({ username }).select({ _id: 0, username: 1, password: 1 }).exec()
 
-  if (user === null) {
+  // Check user found and password correct
+  if (user === null || !await bcrypt.compare(password, user.password)) {
     return createError({ statusCode: 404, statusMessage: "Username or password incorrect" })
   }
+
+  // Update last active
+  await User.updateOne({ username }, { lastActive: new Date() })
 
   // Create new access and refresh tokens
   const accessToken = jwt.sign({ username }, config.ACCESS_SECRET_KEY, { expiresIn: "1h" })
--- server/api/auth/user.post.ts
+++ server/api/auth/user.post.ts
@@ -1,10 +1,15 @@
 import User from "@/server/models/user"
+import bcrypt from "bcrypt"
+
+const saltRounds = 10
 
 export default defineEventHandler(async event => {
   const { name, username, password } = await readBody(event)
   try {
+    // Hash password
+    const hash = await bcrypt.hash(password, saltRounds)
     // Create new user
-    const user = await User.create({ name, username, password })
+    const user = await User.create({ name, username, password: hash })
     return user
   } catch (err: any) {
     // Duplicate error
