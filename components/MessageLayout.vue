d8e200789601b6a272438056c20b237afaa6bbe8
Tanat <ttanat@protonmail.com>
2023-01-05 14:07:04 +0700

    Message layout

---
+++ components/MessageLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="justify-center align-self-center">
+    <span class="text-h6">
+      <v-progress-circular v-if="loading" indeterminate></v-progress-circular>&nbsp;
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  loading: {
+    type: Boolean,
+    required: false,
+    default: false,
+  }
+})
+</script>
+
+<style scoped>
+.v-progress-circular {
+  top: -2.5px;
+}
+</style>
--- pages/logout.vue
+++ pages/logout.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="justify-center align-self-center">
-    <v-progress-circular indeterminate></v-progress-circular>&ensp;<span class="text-h6">Logging out...</span>
-  </div>
+  <MessageLayout :loading="true">
+    Logging out...
+  </MessageLayout>
 </template>
 
 <script setup>
