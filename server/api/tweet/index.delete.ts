1336c73191798395a4a0cb0c93d6f6d395989483
Tanat <ttanat@protonmail.com>
2023-01-14 00:12:09 +0700

    Delete tweet

---
+++ server/api/tweet/index.delete.ts
@@ -0,0 +1,26 @@
+import Tweet from "~~/server/models/tweet"
+import { checkUsername } from "~~/server/utils/query"
+
+export default defineEventHandler(async event => {
+  const username = event.context.user?.username
+  if (!checkUsername(username)) {
+    return createError({ statusCode: 400 })
+  }
+  const { _id } = getQuery(event)
+  if (!_id) {
+    return createError({ statusCode: 400 })
+  }
+
+  const tweet = await Tweet.findOne({ _id }, { _id: 0, isRemoved: 1, isDeleted: 1 })
+    .populate<{ user: { username: string }}>("user", "-_id username")
+    .exec()
+
+  if (!tweet || tweet.isRemoved || tweet.isDeleted || username !== tweet.user.username) {
+    return createError({ statusCode: 400 })
+  }
+
+  // await Tweet.deleteOne({ _id }).exec()
+  await Tweet.updateOne({ _id }, { isDeleted: true }).exec()
+
+  return null
+})
