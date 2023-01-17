b7b6ddd3c4aa1b6dd7abd217aec41beae90a71c7
Tanat <ttanat@protonmail.com>
2023-01-17 15:47:19 +0700

    Bookmarks page

--- pages/bookmarks.vue
+++ pages/bookmarks.vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+  <BaseLayout v-if="$auth.loggedIn()">
+    <TweetList :url="`/api/tweets/bookmarks?username=${$auth.getUsername()}&page=1`" />
+  </BaseLayout>
+</template>
+
+<script setup>
+definePageMeta({
+  middleware: ["auth"]
+})
+</script>
