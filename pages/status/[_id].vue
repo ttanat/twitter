923fc5195ce6e361e07b0f867cf5828a67287653
Tanat <ttanat@protonmail.com>
2023-01-23 19:55:48 +0700

    Fix error: cannot read properties of null

--- pages/status/[_id].vue
+++ pages/status/[_id].vue
@@ -1,6 +1,6 @@
 <template>
   <BaseLayout>
-    <TweetStatus v-if="tweet" :tweet="tweet" @handle-like="handleLike" />
+    <TweetStatus :tweet="tweet" @handle-like="handleLike" />
     <TweetList :url="`/api/tweets/replies?_id=${_id}&before=${new Date().toISOString()}`" />
   </BaseLayout>
 </template>
@@ -9,7 +9,7 @@
 useState("navBarRoute").value = "Tweet"
 const route = useRoute()
 const { _id } = route.params
-const { data: tweet } = await useApiFetch("/api/tweet", { query: { _id } })
+const { data: tweet } = await useApiFetch("/api/tweet", { query: { _id }, server: true })
 
 function handleLike() {
   tweet.value.isLiked = !tweet.value.isLiked
