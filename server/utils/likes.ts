9695d918383f51624faea618a78b165ce88b4d69
Tanat <ttanat@protonmail.com>
2023-01-28 02:17:44 +0700

    Check if user retweeted tweets in feed

--- server/utils/likes.ts
+++ server/utils/likes.ts
@@ -1,3 +1,4 @@
+import Tweet from "~~/server/models/tweet"
 import User from "~~/server/models/user"
 import { H3Event } from "h3"
 import { ci } from "./collations"
@@ -46,8 +47,63 @@ export const checkTweetIsLiked = async (event: H3Event, _id: Types.ObjectId | st
   const isLiked = (
     await User.findOne({ username: event.context.user.username }, {
       _id: 0, likes: { $elemMatch: { $eq: _id }}
-    }).exec()
+    }).collation(ci).exec()
   )?.likes.length === 1
 
   return isLiked
+}
+
+export const checkTweetsAreRetweeted = async (event: H3Event, tweets: any[]): Promise<any[]> => {
+  if (!event.context.user?.username || tweets.length === 0) {
+    return tweets
+  }
+  
+  // Get user
+  const user = await User.findOne({ username: event.context.user.username }, { _id: 1 }).collation(ci).exec()
+  if (!user) {
+    return tweets
+  }
+
+  // Get _id's of tweets
+  const tweet_ids = tweets.map(tweet => tweet._id)
+
+  // Check if user retweeted those tweets
+  const retweetObjs = await Tweet.find(
+    { user: user._id, retweet: { $in: tweet_ids }},
+    { _id: 1 },
+    { limit: 20 },
+  ).exec()
+
+  // Add _id's to set
+  const retweets = new Set(retweetObjs.map(obj => obj._id.toString()))
+
+  // Add user retweets to tweets
+  if (retweets.size > 0) {
+    tweets = tweets.map(tweet => {
+      const isRetweeted = retweets.has(tweet._id.toString())
+      return { ...tweet, isRetweeted }
+    })
+  }
+
+  return tweets
+}
+
+export const checkIsRetweeted = async (event: H3Event, _id: Types.ObjectId | string): Promise<boolean> => {
+  if (!event.context.user?.username) {
+    return false
+  }
+
+  // Get user
+  const user = await User.findOne(
+    { username: event.context.user.username },
+    { _id: 1 },
+  ).collation(ci).exec()
+  if (!user) {
+    return false
+  }
+
+  // Check if user retweet exists
+  const retweet = await Tweet.findOne({ user: user._id, retweet: _id }, { _id: 1 }).exec()
+
+  return !!retweet
 }
