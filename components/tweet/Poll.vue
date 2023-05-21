9c770d82eeface79fd98f7c2392514caa52163f3
Tanat <ttanat@protonmail.com>
2023-05-21 17:03:56 +0700

    Show poll choices

--- components/tweet/Poll.vue
+++ components/tweet/Poll.vue
@@ -4,10 +4,21 @@
       v-for="(choice, i) in choices"
       :class="{ 'my-2': i === 0 || i === choices.length, 'my-3': i !== 0 && i !== choices.length }"
     >
+      <!-- Poll choices -->
+      <div
+        v-if="!voted && timeLeft"
+        @click="selectChoice"
+        class="px-2 poll-choice"
+      >
+        <input type="radio" :id="choice._id" :name="poll._id" :value="choice.choice" class="mr-2">
+        <label :for="choice._id">{{ choice.choice }}</label>
+      </div>
+      <!-- Poll results -->
       <v-progress-linear
+        v-else
         v-model="choice.percentage"
         color="blue-darken-1"
-        height="30"
+        height="27"
         class="rounded no-navigate"
       >
         <span class="ml-2">{{ choice.choice }}</span>
@@ -16,11 +27,9 @@
       </v-progress-linear>
     </div>
     <div class="text-grey no-navigate">
-      {{ poll.totalNumVotes }} votes
-      &ensp;&bull;&ensp;
-      <span :title="expiryDate.toLocaleString()">{{ timeLeft }}</span>
-      &ensp;&bull;&ensp;
-      {{ voted ? "Voted" : "Click to vote" }}
+      <span>{{ poll.totalNumVotes }} votes</span>
+      <span>&ensp;&bull;&ensp;</span>
+      <span :title="expiryDate.toLocaleString()">{{ timeLeft || "Final results" }}</span>
     </div>
   </div>
 </template>
@@ -38,6 +47,14 @@ const choices = ref(props.poll.choices.map(choice => {
   const percentage = Math.round((choice.numVotes / props.poll.totalNumVotes) * 100) || 0
   return { ...choice, percentage }
 }))
+
+function selectChoice(e) {
+  /* For when user doesn't click on input or label */
+  const tmp = e.target.childNodes?.[0]
+  if (tmp?.nodeName === "INPUT") {
+    tmp.click()
+  }
+}
 
 const expiryDate = new Date(props.poll.expiry)
 const timeLeft = computed(() => {
@@ -62,6 +79,16 @@ const timeLeft = computed(() => {
     const n = Math.round(secondsLeft)
     return `${n} second${n > 1 ? "s" : ""} left`
   }
-  return "Final results"
+  return ""
 })
 </script>
+
+<style scoped>
+.poll-choice {
+  border: solid 1px #555;
+  border-radius: 5px;
+  cursor: pointer;
+  padding-top: 2px !important;
+  padding-bottom: 2px !important;
+}
+</style>
