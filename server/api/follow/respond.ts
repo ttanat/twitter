effe6dbd2bd9e5ed0baa83653d5b02b10c86ad45
Tanat <ttanat@protonmail.com>
2023-05-24 20:59:20 +0700

    Use helper function

--- server/api/follow/respond.ts
+++ server/api/follow/respond.ts
@@ -1,5 +1,5 @@
 import User from "~~/server/models/user"
-import { checkFollowMethod, getUsers, handleFollow } from "~~/server/utils/follow"
+import { checkFollowMethod, getUsers, handleDeleteRequest, handleFollow } from "~~/server/utils/follow"
 
 export default defineEventHandler(async event => {
   if (!checkFollowMethod(event)) {
@@ -20,20 +20,23 @@ export default defineEventHandler(async event => {
   }
 
   // Delete follow request (even if follow didn't happen ^)
-  const res2 = await User.bulkWrite([
-    {
-      updateOne: {
-        filter: { _id: currentUser._id },
-        update: { $pull: { followRequestsReceived: targetUser._id }},
-      }
-    },
-    {
-      updateOne: {
-        filter: { _id: targetUser._id },
-        update: { $pull: { followRequestsSent: currentUser._id }},
-      }
-    },
-  ])
+  // const res2 = await User.bulkWrite([
+  //   {
+  //     updateOne: {
+  //       filter: { _id: currentUser._id },
+  //       update: { $pull: { followRequestsReceived: targetUser._id }},
+  //     }
+  //   },
+  //   {
+  //     updateOne: {
+  //       filter: { _id: targetUser._id },
+  //       update: { $pull: { followRequestsSent: currentUser._id }},
+  //     }
+  //   },
+  // ])
+  // Swap currentUser and userToDeleteRequest around
+  const res2 = await handleDeleteRequest(targetUser._id, currentUser._id)
 
-  return res.ok && res2.modifiedCount === 2 ? null : createError({ statusCode: 500, statusMessage: "Oops, something went wrong." })
+  // return res.ok && res2.modifiedCount === 2 ? null : createError({ statusCode: 500, statusMessage: "Oops, something went wrong." })
+  return res.ok && res2.ok ? null : createError({ statusCode: 500, statusMessage: "Oops, something went wrong." })
 })
