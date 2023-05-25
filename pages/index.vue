c80e66c628c0bf350ca06341efcf7581abc7e5a9
Tanat <ttanat@protonmail.com>
2023-05-25 18:06:27 +0700

    Home feed

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
   <BaseLayout>
-    <TweetFeedList />
+    <TweetFeedList :url="url" />
   </BaseLayout>
 </template>
 
 <script setup>
 useHead({ title: "Home" })
 useState("navBarRoute").value = "Home"
+
+const { $auth: auth } = useNuxtApp()
+
+const url = ref(auth.loggedIn() ? `/api/tweets?before=${new Date().toISOString()}` : "/api/tweets/explore")
 </script>
--- server/api/tweets/index.get.ts
+++ server/api/tweets/index.get.ts
@@ -1,6 +1,8 @@
 import Tweet from "@/server/models/tweet"
 import User from "@/server/models/user"
+import { ci } from "@/server/models/user"
 import { parseRetweets, checkLikesAndRetweets, getNextUrl } from "~~/server/utils/feed"
+import { checkDateString, checkUsername } from "~~/server/utils/query"
 
 /*
   Routes:
@@ -24,7 +26,27 @@ import { parseRetweets, checkLikesAndRetweets, getNextUrl } from "~~/server/util
 */
 
 export default defineEventHandler(async event => {
-  const tweets = await Tweet.find()
+  const username = event.context.user?.username
+  const { before } = getQuery(event)
+  if (!checkUsername(username) || !checkDateString(before)) {
+    return createError({ statusCode: 400 })
+  }
+
+  const user = await User.findOne({ username }, { _id: 1, following: 1 }).collation(ci).exec()
+  if (!user) {
+    return createError({ statusCode: 400 })
+  }
+
+  const tweets = await Tweet.find(
+    {
+      user: { $in: user.following },
+      timestamp: { $lt: before },
+      isDeleted: false,
+      isRemoved: false,
+    },
+    null,
+    { sort: { timestamp: -1 }},
+  )
     .populate("user", "-_id username name image")
     .populate({
       path: "retweet",
