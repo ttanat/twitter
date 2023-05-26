1423d0a3d132c87cca2edba0096e93883ece1bde
Tanat <ttanat@protonmail.com>
2023-05-26 22:33:26 +0700

    Show only replies that user is allowed to see

--- server/api/tweets/replies.get.ts
+++ server/api/tweets/replies.get.ts
@@ -1,6 +1,7 @@
 import Tweet from "~~/server/models/tweet"
 import { checkDateString, checkId } from "~~/server/utils/query"
 import { checkLikesAndRetweets, getNextUrl } from "~~/server/utils/feed"
+import User, { ci } from "~~/server/models/user"
 
 export default defineEventHandler(async event => {
   const { _id, before } = getQuery(event)
@@ -8,8 +9,43 @@ export default defineEventHandler(async event => {
     return createError({ statusCode: 400 })
   }
 
+  // Get parent tweet
+  const parent = await Tweet
+    .findOne({ _id }, { _id: 1, isPrivate: 1 })
+    .populate("user", "_id isPrivate")
+    .exec()
+  if (!parent) {
+    return createError({ statusCode: 400 })
+  }
+
+  // Get current user
+  const username = event.context.user?.username
+  const user = username ? await User.findOne({ username }, { following: 1 }).collation(ci).exec() : null
+
+  if (parent.isPrivate) {
+    if (!user) {
+      return createError({ statusCode: 403 })
+    }
+    // Check if user can view parent tweet (and therefore replies)
+    if (!user.following.includes(parent.user._id)) {
+      return createError({ statusCode: 403 })
+    }
+  }
+
+  // If signed in, tweet has to be public or user's own tweet or user has to follow tweeter
+  // If not, then only allowed if reply is public
+  const query = user ? { $or : [{ isPrivate: false }, { user: user._id }, { user: { $in: user.following }}]}
+                       : { isPrivate: false }
+
   const replies = await Tweet
-    .find({ parent: _id, timestamp: { $lt: before } }, {}, { limit: 20, sort: { timestamp: -1 } })
+    .find({
+      parent: _id,
+      // retweet: null,
+      timestamp: { $lt: before },
+      ...query,
+      isDeleted: false,
+      isRemoved: false,
+    }, null, { limit: 20, sort: { timestamp: -1 } })
     .populate("user", "-_id username name image")
     .exec()
 
