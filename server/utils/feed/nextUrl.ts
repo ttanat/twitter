9f6dadb7de26bea43b4b34ecf79a493325693861
Tanat <ttanat@protonmail.com>
2023-06-01 00:23:44 +0700

    Get URL without query parameters

--- server/utils/feed/nextUrl.ts
+++ server/utils/feed/nextUrl.ts
@@ -1,5 +1,6 @@
 import { H3Event } from "h3"
 
 export const getNextUrl = (event: H3Event, params: Record<string, string>): string => {
-  return `${event.node.req.url}?${new URLSearchParams(params).toString()}`
+  // Get URL up to "?" and add query parameters from params
+  return `${(event.node.req.url)!.match(/^[^?]+/)}?${new URLSearchParams(params).toString()}`
 }
